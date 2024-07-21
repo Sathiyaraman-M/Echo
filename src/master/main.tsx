f3d04e1f1807c2309b7e100b70b90de759613190
SathiyaramanM <Sathiyaraman2003@gmail.com>
2024-07-21 13:34:47 +0530

    Remove Master Window code

--- src/master/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import '../style.css'
-import App from './App.tsx';
-import {Container, createRoot} from "react-dom/client";
-
-const root = createRoot(document.getElementById('root') as Container);
-root.render(<App />);
